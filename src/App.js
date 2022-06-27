75591ffa0a75e9672a39c5dace496f0cda0e793f
Elijah Malabuyo <elijahbmalabuyo@gmail.com>
2022-06-27 11:58:57 -0700

    edit nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react"
 import './App.js';
 import Navbar from "./components/nav"
+import Intro from "./components/intro.js";
+import jump from './images/jump.JPG'
+
 
 function App() {
+
   return (
+    <div>
       <Navbar />
+      <div className="Home">
+        <h1 className="Home--text">WELCOME</h1>
+      </div>
+      <Intro />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 
+
 const root = document.getElementById('root');
 
 ReactDOM.render(<App />, root)

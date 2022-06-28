77e0060bfa790d6f5b1f26e9143c75ec1cf73fd9
Elijah Malabuyo <elijahbmalabuyo@gmail.com>
2022-06-28 00:14:12 -0700

    added projects

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.js';
 import Navbar from "./components/nav"
 import Intro from "./components/intro.js";
 import jump from './images/jump.JPG'
-
+import Bubble from "./components/projects.js";
+import project_info from "./project_info.js";
 
 function App() {
 
+  const projectBubbles = project_info.map(thing => {
+    return <Bubble 
+            thing = {thing}
+    />
+  })
   return (
     <div>
       <Navbar />
       <div className="Home">
-        <h1 className="Home--text">WELCOME</h1>
       </div>
       <Intro />
+      <div className="projects--cont">
+        <h2 className="projects--title">Projects</h2>
+        {projectBubbles}
+      </div>
     </div>
   );
 }

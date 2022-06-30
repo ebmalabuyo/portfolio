d8d6e6f6ad2bf38f6bad3663e9f56426592a2c79
Elijah Malabuyo <elijahbmalabuyo@gmail.com>
2022-06-29 21:13:16 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,37 @@ import Navbar from "./components/nav"
 import Intro from "./components/intro.js";
 import Bubble from "./components/projects.js";
 import project_info from "./project_info.js";
+import Work from './components/work'
+import work_info from "./work_info.js";
+import "./components/work_style.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
 
+
+  //map through the bubble components
   const projectBubbles = project_info.map(thing => {
     return <Bubble 
             thing = {thing}
     />
   })
+
+
+
   return (
     <div>
       <Navbar />
       <div className="Home">
+        {/* <h1 className="welcome">Welcome!</h1> */}
       </div>
       <Intro />
+      <hr></hr>
       <div className="projects--cont">
-        <h2 className="projects--title">Projects</h2>
+        <h2 className="projects--title">Projects <span role = 'img'>🖥️</span></h2>
         {projectBubbles}
+      </div>
+      <div className="work--cont">
+      <i className="fa-solid fa-arrow-left-long"></i>
       </div>
     </div>
   );
